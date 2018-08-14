f52a45929b214a882cabb522e9d549b5952ef399
Holly Grimm <holly.grimm@gmail.com>
2018-08-14 09:38:57 -0600

    add base classes

---
+++ base/base_data_loader.py
@@ -0,0 +1,10 @@
+
+class BaseDataLoader(object):
+    def __init__(self, config):
+        self.config = config
+
+    def get_train_data(self):
+        raise NotImplementedError
+
+    def get_test_data(self):
+        raise NotImplementedError
---
+++ base/base_trainer.py
@@ -0,0 +1,9 @@
+
+class BaseTrain(object):
+    def __init__(self, model, data, config):
+        self.model = model
+        self.data = data
+        self.config = config
+
+    def train(self):
+        raise NotImplementedError
