3bb9108597b63bc36c47013b870cbecfbfdfb6e1
Holly Grimm <holly.grimm@gmail.com>
2018-08-14 10:51:41 -0600

    initial commit

---
+++ main.py
@@ -0,0 +1,36 @@
+import sys
+from utils.utils import get_args, process_config, create_dirs
+from data_loader.wikiart_data_loader import WikiArtDataLoader
+from models.resnet50_attr_model import ResNet50AttrModel
+from trainers.resnet_50_trainer import ResNet50ModelTrainer
+
+# TODO: Add proper logging
+
+def main():
+    # get json configuration filepath from the run argument
+    # process the json configuration file
+    try:
+        args = get_args()
+        config, log_dir, checkpoint_dir = process_config(args.config)
+    except:
+        print('missing or invalid arguments')
+        print('Unexpected error:', sys.exc_info()[0])
+
+    # create the experiment directories
+    create_dirs([log_dir, checkpoint_dir])
+
+    print('Create the data generator')
+    data_loader = WikiArtDataLoader(config)
+
+    print('Create the model')
+    model = ResNet50AttrModel(config)
+    print('model ready loading data now')
+
+    print('Create the trainer')
+    trainer = ResNet50ModelTrainer(model.model, data_loader.get_train_data(), data_loader.get_test_data(), config, log_dir, checkpoint_dir)
+
+    print('Start training the model.')
+    trainer.train()
+
+if __name__ == '__main__':
+    main()
