ddefdd5dbb61294d84c630c9f460ecb65d89621f
Holly Grimm <holly.grimm@gmail.com>
2018-08-26 15:50:26 -0600

    test more values

--- tests/art_composition_cnn_tests.py
+++ tests/art_composition_cnn_tests.py
@@ -17,17 +17,35 @@ class TestArtCompositionCNN(unittest.TestCase):
         harmony_encoder = LabelEncoder()
         harmony_encoder.fit(config['harmonies'])   
 
-        color = 'orange'
+        color = 'blue'
         color_enc = color_encoder.transform([color])
-        self.assertListEqual(color_enc.tolist(), [9])
+        self.assertListEqual(color_enc.tolist(), [1])
         color_onehot = to_categorical(color_enc, num_classes=len(color_encoder.classes_))
-        self.assertListEqual(color_onehot.tolist(), [[0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0]])
+        self.assertListEqual(color_onehot.tolist(), [[0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]])
+
+        color = 'green-cyan'
+        color_enc = color_encoder.transform([color])
+        self.assertListEqual(color_enc.tolist(), [6])
+        color_onehot = to_categorical(color_enc, num_classes=len(color_encoder.classes_))
+        self.assertListEqual(color_onehot.tolist(), [[0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0]])        
+
+        color = 'yellow'
+        color_enc = color_encoder.transform([color])
+        self.assertListEqual(color_enc.tolist(), [12])
+        color_onehot = to_categorical(color_enc, num_classes=len(color_encoder.classes_))
+        self.assertListEqual(color_onehot.tolist(), [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]])        
 
         harmony = 'analogous'
         harmony_enc = harmony_encoder.transform([harmony])
         self.assertListEqual(harmony_enc.tolist(), [0])
         harmony_onehot = to_categorical(harmony_enc, num_classes=len(harmony_encoder.classes_))
         self.assertListEqual(harmony_onehot.tolist(), [[1, 0, 0, 0, 0, 0]])
+
+        harmony = 'triadic'
+        harmony_enc = harmony_encoder.transform([harmony])
+        self.assertListEqual(harmony_enc.tolist(), [5])
+        harmony_onehot = to_categorical(harmony_enc, num_classes=len(harmony_encoder.classes_))
+        self.assertListEqual(harmony_onehot.tolist(), [[0, 0, 0, 0, 0, 1]])        
         
 
 if __name__ == '__main__':
